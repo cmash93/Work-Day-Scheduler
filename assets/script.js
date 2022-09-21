e20257c98a5c13dc756fe81c328703c4eb5e16af
cmash93 <cmash93@gmail.com>
2022-09-20 22:30:17 -0500

    started js, but i think i'm going to use jquery

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,17 @@
 // Function to get current time
 // Function to determine if time is in the past, present, future and add classes to color code it 
+// Function to save once save button is pressed.
+// Function actually save item to local storage. 
+
+let currentDay = document.querySelector('#currentDay');
+let timeBlock = document.querySelector('.time-block');
+let description = document.querySelector('.description');
+let saveBtn = document.querySelector('#saveBtn');
+
+saveInput
+
+saveBtn.addEventListener('click', function() {
+
+})
+
+
